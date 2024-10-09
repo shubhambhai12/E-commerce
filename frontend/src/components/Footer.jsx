b1bc930d0a36bd755c45f8119e9fa62cad25d198
shubhampatel <shubhampatel0725@gmail.com>
2024-10-09 13:40:40 +0530

    change

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Footer = () => {
+    return (
+        <div>
+            <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
+
+                <div className=''>
+                    <img className='mb-5 w-32' src={assets.logo} alt="" />
+                    <p className='w-full md:w-2/3 text-gray-600'>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. At nihil cum sit natus, ex consequatur vel cupiditate facere ab corporis! Magni, aliquam. Esse porro officiis, vero possimus exercitationem nulla voluptas delectus sed aspernatur nihil, odit aperiam a et repellendus cupiditate repellat obcaecati praesentium dolorem nesciunt voluptatum illo. Natus, modi vitae.
+                    </p>
+                </div>
+
+                <div>
+                    <p className='text-xl font-medium mb-5'>COMPANY</p>
+                    <ul className='flex flex-col gap-1 text-gray-600'>
+                        <li>Home</li>
+                        <li>About us</li>
+                        <li>Delivery</li>
+                        <li>Privacy Policy</li>
+                    </ul>
+                </div>
+
+                <div>
+                    <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+                    <ul className='flex flex-col gap-1 text-gray-600'>
+                        <li>+1-212-456-7890</li>
+                        <li>contact@foreveryou.com</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div className=''>
+                <hr />
+                <p className='py-5 text-sm text-center'>
+                    Copyright 2024@ forever.com - All Right Reserved.
+                </p>
+            </div>
+
+        </div>
+    )
+}
+
+export default Footer
